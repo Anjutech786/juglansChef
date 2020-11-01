a54ea45fa77769087450ab3b85f290b06b665c55
root <root@ip-172-31-32-218.ap-south-1.compute.internal>
2020-11-01 18:38:15 +0000

    November First Commit

--- apache-cookbook/recipes/apache-recipe.rb
+++ apache-cookbook/recipes/apache-recipe.rb
@@ -28,6 +28,12 @@ action :create
 end
 
 
+
+file 'var/www/html/home.html' do
+content '<html><head><style>h1{background-color:blue;color:white}</style></head><body><h1>Hello this is Html File</h1></body></html>'
+action :create
+end
+
 service 'httpd' do
 action [:enable, :start]
 end
--- test-cookbook/recipes/test-recipe.rb
+++ test-cookbook/recipes/test-recipe.rb
@@ -10,3 +10,10 @@ content 'Welcome to juglans new changes 31 October 2020 '
 action :create
 end
 
+
+
+
+file '/my1Novemberfile' do
+content 'Hello this is first file in november month'
+action :create
+end
